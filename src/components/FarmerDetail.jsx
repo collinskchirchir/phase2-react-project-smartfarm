e1e653df69c09e0f6bd773d035acc28e0b410eb0
Collins Chirchir <collinskchirchir@gmail.com>
2023-01-27 00:07:59 +0300

    Styled Farmer Detail

--- src/components/FarmerDetail.jsx
+++ src/components/FarmerDetail.jsx
@@ -19,14 +19,24 @@ const FarmerDetail = () => {
     if (!farmer) return <h2>Loading...</h2>
     
   return (
-    <div>
-      <h1>{fname} {lname}</h1>
-      <p>{dob}</p>
-      <p>{nat_id}</p>
-      <p>{crop_acre}</p>
-      <p>{status}</p>
-      <p>{active}</p>                
+    <>
+   
+    <div class="card container mt-5">
+      <h5 class="card-header">{status}</h5>
+      <div class="card-body">
+        <h5 class="card-title">{fname} {lname}</h5>
+        <p class="card-text">Farmer is ready for MSG</p>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">National ID: {nat_id}</li>
+          <li class="list-group-item">Date of Birth: {dob}</li>
+          <li class="list-group-item">Farm Size(acre): {crop_acre}</li>
+          <li class="list-group-item">Active: {active}</li>
+        </ul>
+        <button class="btn btn-danger me-4">Delete Farmer</button>
+        <button class="btn btn-primary">Status: NEXT STATUS</button>
+      </div>
     </div>
+  </>
   );
 };
 
